b50c723b843a8864d0e1c390504b54fa8255e028
EpicViv <epicviv987@gmail.com>
2020-12-07 14:04:23 -0600

    Finished 12/7 notes on loop structures

--- Conditions.java
+++ Conditions.java
@@ -223,7 +223,123 @@ public class Conditions
           a--; //decrement as well!
      }
 
+     public static void whileExample(){
+          /*
+
+          while loop:
+
+          evaluates boolean expression
+               if true, executes the body of the loop and then re-evaluates the condition
+               if false, skips the body and continues
+           */
+
+          int count = 1;                     // initialization
+
+          while (count <= 5){                // condition
+               Tools.println(count);         // body
+
+               count++;                      // update the loop variable
+          }
+
+          Tools.println("Done.");
+     }
+
+     public static void whileExample2(){
+          /*
+
+          This is an infinite loop!
+
+          A better condition would be:
+               while(count < 50)
+           */
+          int count = 1;
+          while(count != 50){
+               Tools.println(count);
+               count += 2;
+          }
+
+          Tools.println("Done.");
+     }
+
+     public static void forExample(){
+          /*
+
+          for loop:
+
+               Three parts of the for loop statement:
+                    1. initialization - executed once
+                    2. condition - boolean expression evaluated at the *start* of each iteration
+                    3. update the loop variable - executed at the *end* of each iteration *before* evaluating the condition again
+           */
+
+          for (int count = 1; count <= 5; count++){
+               Tools.println(count);
+          }
+
+          Tools.println("Complete.");
+
+          /*
+
+          Variables declared witht he for statement are scoped to the for statement and its body
+           */
+          //Tools.println("Final value of count: " + count);
+     }
+
+     public static void forExample2(){
+          int count = 1;
+          for (; count <= 5; count++){
+               Tools.println(count);
+          }
+
+          Tools.println("Complete.");
+
+          Tools.println("Final value of count: " + count);
+     }
+
+     public static void offByOne(){
+          // We want to print five "*"
+
+          /*
+
+          The infamous off-by-one error is common with for loops executing one too many or one too few times.
+
+          Carefully ask:
+               1. Should the initial value start at 0 or 1?    (usually we start at 0 when counting a set number)
+               2. Should the condition operator be < or <=?    (usually we use < when counting a set number)
+
+          "Think! Don't compile and try at random!"
+           */
+
+          for (int i = 0; i <= 5; i++){ // should be i < 5
+               Tools.print("* ");
+          }
+
+          Tools.print("\n1 2 3 4 5");
+     }
+
+     public static void doExample(){
+          /*
+
+          do loop (do-while loop)
+
+               1. executes the body of the loop
+               2. evaluates the condition
+                    if true, executes the body of the loop again
+                    if false, continues execution after the loop
+           */
+
+          int count = 1;
+
+          do{
+               Tools.println(count);         // body
+               count++;                      // update the loop variable
+          }
+          while(count <= 5);
+
+          Tools.println("Complete.");
+     }
+
      public static void main(String[] args){
-          stringExample();
+          doExample();
      }
 }
